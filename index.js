af337aded1b7fad718309c897d44f8b6763e700b
Gheorghii Iabanji <georgiy.is.ninja.blockchain@gmail.com>
2019-06-02 19:02:00 +0300

    Arch: add config and file parser

---
+++ index.js
@@ -0,0 +1,32 @@
+//let stdin = process.openStdin();
+
+// process.argv.forEach((val, index) => {
+//   console.log(`${index}: ${val}`);
+// });
+
+let data = {
+  'input': [],
+  'vars': [],
+  'output': [],
+};
+
+let lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('file.txt')
+});
+
+lineReader.on('line', (line) => {
+  if (line == '') { return; }
+  if(line[0] == '=') { 
+    for(let i = 1; i < line.length; i++){
+      data.vars.push(line[i]);
+    };
+    return;
+   }
+  if(line[0] == '?'){
+    for(let i = 1; i < line.length; i++){
+      data.output.push(line[i]);
+    };
+    return;
+  }
+  data.input.push(line);
+});
