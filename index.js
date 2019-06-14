c43fd54e5ba01aea8beccc00dc5203d22be6ee47
Maksym Kaliberda <maxkaliberda1@gmail.com>
2019-06-14 19:57:40 +0300

    add Formatter

--- index.js
+++ index.js
@@ -1,74 +1,101 @@
-//let stdin = process.openStdin();
-
-// process.argv.forEach((val, index) => {
-//   console.log(`${index}: ${val}`);
-// });
-
 const data = {
   'input': [], //{'left':'A|B+C', 'right': 'E'},
   'vars': {},
   'output': ['E'], //['E'] - переменные котрые необходимо найти
-};
+}
+
+const operators = {
+  '+': (x, y) => x && y,
+  '|': (x, y) => x || y,
+  '^': (x, y) => x || y && x != y,
+  '!': (x) => !x
+}
 
 const excludeSymbols = [' ', '\t', '\n']
 
-const factCheaker = (lineArray) => {
-  lineArray.forEach(element => {
-    // handler if got line with Fact
-    if (element.startsWith('=')) {
-      element = element.replace(/['=']/g, '');
-      // check to Uppercase Character  A, B, C, D
-      if (!/^[A-Z]*$/.test(element)) {
-        //TODO can add custom validation
-        throw "Note A-b character if {}"
-      }
-      const factsArray = [...element];
-      factsArray.forEach(fact => {
-        data.vars[fact] = true;
-      });
-    }
-    // handle if ask a condition 
-    else if () {
+class Formatter {
 
+  testAlphabet(element) {
+    if (!/^[A-Z]*$/.test(element)) {
+      //TODO can add custom validation
+      throw `Not A-b character in ${element}`
     }
-  });
-}
-
-const lineParser = (line) => {
-  line = line.split('#')[0];
-  line = line.replace(/([' ', '\t', '\n'])/g, '');
-  if (line) {
-    return line
+    return [...element]
   }
-  return null;
-};
+
+  lineToStore(line) {
+    line = line.split('#')[0]
+    line = line.replace(/([' ', '\t', '\n'])/g, '')
+    if (line) {
+      return line
+    }
+    return null
+  }
+
+  rulesFormatter(element) {
+
+  }
+
+  lineFormatter(lineArray) {
+    lineArray.forEach(element => {
+      // handler if a Fact
+      if (element.startsWith('=')) {
+        element = element.replace(/['=']/g, '')
+        this.testAlphabet(element).forEach(fact => {
+          data.vars[fact] = true
+        })
+      }
+      // handle if a condition 
+      else if (element.indexOf('?') != -1) {
+        element = element.replace(/['?']/g, '')
+        data.output = this.testAlphabet(element = element)
+      }
+      // handle if a rules 
+      else if (element.indexOf('=>') != -1) {
+        rulesFormatter(element = element)
+      }
+    })
+  }
+}
 
 const lineReader = require('readline').createInterface({
   input: require('fs').createReadStream(process.argv[2])
-});
+})
+
 
 lineReader.on('line', (line) => {
-  let lineArray = [];
-  let formatLine = null;
-  formatLine = lineParser(line=line);
+  let lineArray = []
+  let formatLine = null
+  // formatLine = lineToStore(line=line)
+  formatter = new Formatter()
+  formatLine = formatter.lineToStore(line = line)
   if (formatLine) {
-    lineArray.push(formatLine);
+    lineArray.push(formatLine)
   }
-  factCheaker(lineArray=lineArray);
-  // console.log(lineArray);
-  // console.log('store', fileData);
-  // if (line == '') { return; }
+  try {
+    formatter.lineFormatter(lineArray = lineArray)
+  } catch (error) {
+    console.error(error)
+  }
+  // console.log(lineArray)
+  // console.log('store', fileData)
+  // if (line == '') { return }
   // if(line[0] == '=') { 
-  //   for(let i = 1; i < line.length; i++){
-  //     data.vars.push(line[i]);
-  //   };
-  //   return;
+  //   for(let i = 1 i < line.length i++){
+  //     data.vars.push(line[i])
+  //   }
+  //   return
   //  }
   // if(line[0] == '?'){
-  //   for(let i = 1; i < line.length; i++){
-  //     data.output.push(line[i]);
-  //   };
-  //   return;
+  //   for(let i = 1 i < line.length i++){
+  //     data.output.push(line[i])
+  //   }
+  //   return
   // }
-  // data.input.push(line);
-});
+  // data.input.push(line)
+})
+
+
+lineReader.on('close', (line) => {
+  console.log(data)
+})
